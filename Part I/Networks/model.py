9e7889a7f82932815473d0f4dae6e2a1e0c71009
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2024-11-12 12:28:45 +0100

    Add YAML configuration files for experiments and update .gitignore for dataset paths

---
+++ Part I/Networks/model.py
@@ -0,0 +1,219 @@
+from Dataset.dataLoader import *
+from Dataset.makeGraph import *
+from Networks.Architectures.basicNetwork import *
+import albumentations as A
+
+import numpy as np
+np.random.seed(2885)
+import os
+import copy
+from rich.progress import Progress
+
+import torch
+torch.manual_seed(2885)
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import torch.nn as nn
+import torch.optim
+
+
+# --------------------------------------------------------------------------------
+# CREATE A FOLDER IF IT DOES NOT EXIST
+# INPUT: 
+#     - desiredPath (str): path to the folder to create
+# --------------------------------------------------------------------------------
+def createFolder(desiredPath): 
+    if not os.path.exists(desiredPath):
+        os.makedirs(desiredPath)
+
+
+######################################################################################
+#
+# CLASS DESCRIBING THE INSTANTIATION, TRAINING AND EVALUATION OF THE MODEL 
+# An instance of Network_Class has been created in the main.py file
+# 
+######################################################################################
+
+class Network_Class: 
+    # --------------------------------------------------------------------------------
+    # INITIALISATION OF THE MODEL
+    # INPUTS: 
+    #     - param (dic): dictionnary containing the parameters defined in the 
+    #                    configuration (yaml) file
+    #     - imgDirectory (str): path to the folder containing the images 
+    #     - maskDirectory (str): path to the folder containing the masks
+    #     - resultsPath (str): path to the folder containing the results of the 
+    #                          experiement
+    # --------------------------------------------------------------------------------
+    def __init__(self, param, imgDirectory, maskDirectory, resultsPath):
+        # ----------------
+        # USEFUL VARIABLES 
+        # ----------------
+        self.imgDirectory  = imgDirectory
+        self.maskDirectory = maskDirectory
+        self.resultsPath   = resultsPath
+        self.epoch         = param["TRAINING"]["EPOCH"]
+        self.device        = param["TRAINING"]["DEVICE"]
+        self.lr            = param["TRAINING"]["LEARNING_RATE"]
+        self.batchSize     = param["TRAINING"]["BATCH_SIZE"]
+
+        # -----------------------------------
+        # NETWORK ARCHITECTURE INITIALISATION
+        # -----------------------------------
+        # self.model = Net(param).to(self.device)
+        self.model = UNetLight(param).to(self.device)
+        # -------------------
+        # TRAINING PARAMETERS
+        # -------------------
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+
+        # ----------------------------------------------------
+        # DATASET INITIALISATION (from the dataLoader.py file)
+        # ----------------------------------------------------
+        self.dataSetTrain    = OxfordPetDataset(imgDirectory, maskDirectory, "train", param)
+        self.dataSetVal      = OxfordPetDataset(imgDirectory, maskDirectory, "val",   param)
+        self.dataSetTest     = OxfordPetDataset(imgDirectory, maskDirectory, "test",  param)
+        self.trainDataLoader = DataLoader(self.dataSetTrain, batch_size=self.batchSize, shuffle=True,  num_workers=4)
+        self.valDataLoader   = DataLoader(self.dataSetVal,   batch_size=self.batchSize, shuffle=False, num_workers=4)
+        self.testDataLoader  = DataLoader(self.dataSetTest,  batch_size=self.batchSize, shuffle=False, num_workers=4)
+
+
+    # ---------------------------------------------------------------------------
+    # LOAD PRETRAINED WEIGHTS (to run evaluation without retraining the model...)
+    # ---------------------------------------------------------------------------
+    def loadWeights(self): 
+        self.model.load_state_dict(torch.load(self.resultsPath + '/_Weights/wghts.pkl', weights_only = True))
+
+    # -----------------------------------
+    # TRAINING LOOP (fool implementation)
+    # -----------------------------------
+    def train(self):
+
+        losses_train = []
+        losses_val   = []
+    
+        with Progress() as progress:
+            epoch_task = progress.add_task("[cyan]Epoch Progress", total=self.epoch)
+            
+            for epoch in range(self.epoch):
+    
+                batch_task = progress.add_task(f"[green]Training Epoch {epoch + 1}/{self.epoch}", total=len(self.trainDataLoader))
+                
+                # Training loop
+                self.model.train()  # Set model to training mode
+                train_loss = 0.0
+                for images, GT, resizedImg in self.trainDataLoader:
+                    images, GT = images.to(self.device), GT.to(self.device)
+
+                    outputs = self.model(images).squeeze(1)
+                    loss = self.criterion(outputs, GT)
+
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                    train_loss += loss.item()
+
+                    # Update the batch progress bar
+                    progress.update(batch_task, advance=1)
+                
+            
+                train_loss /= len(self.trainDataLoader)
+                progress.remove_task(batch_task)
+
+                # Validation loop
+                self.model.eval()  # Set model to evaluation mode
+                val_loss = 0.0
+   
+                val_batch_task = progress.add_task(f"[blue]Validating Epoch {epoch + 1}/{self.epoch}", total=len(self.valDataLoader))
+                
+                with torch.no_grad():
+                    for images, GT, resizedImg in self.valDataLoader:
+                        images, GT = images.to(self.device), GT.to(self.device)
+
+                        outputs = self.model(images).squeeze(1)
+                        loss = self.criterion(outputs, GT)
+                        val_loss += loss.item()
+
+   
+                        progress.update(val_batch_task, advance=1)
+
+
+                # Calculate average validation loss 
+                val_loss /= len(self.valDataLoader)
+
+                progress.remove_task(val_batch_task)
+                progress.update(epoch_task, advance=1)
+
+                losses_train.append(train_loss)
+                losses_val.append(val_loss)
+                # Print summary for the epoch
+                print(f'Epoch {epoch+1}/{self.epoch}, '
+                    f'Train Loss: {train_loss:.4f}, '
+                    f'Validation Loss: {val_loss:.4f}')
+
+
+        np.savez(self.resultsPath + '/learning_curve.npz', losses_train=losses_train, losses_val=losses_val)
+    
+        # Save the model weights
+        wghtsPath = self.resultsPath + '/_Weights/'
+        createFolder(wghtsPath)
+        torch.save(self.model.state_dict(), wghtsPath + '/wghts.pkl')
+
+
+
+    # -------------------------------------------------
+    # EVALUATION PROCEDURE (ultra basic implementation)
+    # -------------------------------------------------
+    def evaluate(self):
+        self.model.train(False)
+        self.model.eval()
+        
+        # Qualitative Evaluation 
+        allInputs, allPreds, allGT = [], [], []
+        for (images, GT, resizedImg) in self.testDataLoader:
+            images      = images.to(self.device)
+        
+            predictions = self.model(images)
+            # print(predictions.shape)
+            images, predictions = images.to('cpu'), predictions.to('cpu')
+
+            allInputs.extend(resizedImg.data.numpy())
+            allPreds.extend(predictions.data.numpy())
+            allGT.extend(GT.data.numpy())
+
+        allInputs = np.array(allInputs)
+        allPreds  = np.array(allPreds)
+        allGT     = np.array(allGT)
+
+        showPredictions(allInputs, allPreds, allGT, self.resultsPath)
+
+        # Quantitative Evaluation
+        thresholds = np.linspace(0,1,19)
+        TPR = np.zeros_like(thresholds)
+        FPR = np.zeros_like(thresholds)
+        Tot = 0
+        NumPos = 0
+        for (images, GT, resizedImg) in self.testDataLoader:
+            images      = images.to(self.device)
+        
+            predictions = self.model(images)
+            # print(predictions.shape)
+            images, predictions = images.to('cpu'), predictions.to('cpu')
+
+            predictions = np.squeeze(predictions.data.numpy())
+            predictions = 1 / (1 + np.exp(-predictions))
+            GT = GT.data.numpy()
+            Tot += GT.size
+            NumPos += np.sum(GT)
+            
+            for (i,t) in enumerate(thresholds):
+                mask = predictions > t
+                TPR[i] += np.sum((mask == 1) & (GT == 1))
+                FPR[i] += np.sum(mask > GT)
+        
+        TPR /= NumPos
+        FPR /= Tot - NumPos
+        np.savez(self.resultsPath + '/ROC_curve.npz', TPR=TPR, FPR=FPR)
+
---
+++ Part II/Networks/model.py
@@ -0,0 +1,172 @@
+from Dataset.dataLoader import *
+from Dataset.makeGraph import *
+from Networks.Architectures.basicNetwork import *
+import albumentations as A
+import cv2
+from scipy.stats import entropy
+from albumentations.pytorch import ToTensorV2
+
+import numpy as np
+np.random.seed(2885)
+import os
+import copy
+from rich.progress import Progress
+
+import torch
+torch.manual_seed(2885)
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import torch.nn as nn
+import torch.optim
+
+
+def createFolder(desiredPath): 
+    if not os.path.exists(desiredPath):
+        os.makedirs(desiredPath)
+
+
+class Network_Class: 
+
+    def __init__(self, param, imgDirectory, maskDirectory, resultsPath):
+        # ----------------
+        # USEFUL VARIABLES 
+        # ----------------
+        self.imgDirectory  = imgDirectory
+        self.maskDirectory = maskDirectory
+        self.resultsPath   = resultsPath
+        self.epoch         = param["TRAINING"]["EPOCH"]
+        self.device        = param["TRAINING"]["DEVICE"]
+        self.lr            = param["TRAINING"]["LEARNING_RATE"]
+        self.batchSize     = param["TRAINING"]["BATCH_SIZE"]
+        
+        # -----------------------------------
+        # NETWORK ARCHITECTURE INITIALISATION
+        # -----------------------------------
+        # self.model = Net(param).to(self.device)
+        self.model = UNetLight(param).to(self.device)
+        # -------------------
+        # TRAINING PARAMETERS
+        # -------------------
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+
+        # ----------------------------------------------------
+        # DATASET INITIALISATION (from the dataLoader.py file)
+        # ----------------------------------------------------
+        self.dataSetTrain    = OxfordPetDataset(imgDirectory, maskDirectory, "train", param)
+        self.dataSetVal      = OxfordPetDataset(imgDirectory, maskDirectory, "val",   param)
+        self.dataSetTest     = OxfordPetDataset(imgDirectory, maskDirectory, "test",  param)
+        self.trainDataLoader = DataLoader(self.dataSetTrain, batch_size=self.batchSize, shuffle=True,  num_workers=4)
+        self.valDataLoader   = DataLoader(self.dataSetVal,   batch_size=self.batchSize, shuffle=False, num_workers=4)
+        self.testDataLoader  = DataLoader(self.dataSetTest,  batch_size=self.batchSize, shuffle=False, num_workers=4)
+
+
+        self.transforms = A.ReplayCompose([
+            A.Rotate(limit=(-35, 35), p=0.5),
+            # A.Affine(rotate=(-35, 35), translate_px = (-10, 10), shear = (-5, 5), scale = (0.9, 1.0), p=0.5),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5), 
+            A.ToGray(p=0.5),
+        ], additional_targets={'mask': 'mask'})
+
+    
+    def loadWeights(self): 
+        self.model.load_state_dict(torch.load(self.resultsPath + '/_Weights/wghts.pkl', weights_only = True))
+
+
+    def inverse_transform(self, transformed_mask, replay_data):
+        transformed_mask = transformed_mask.squeeze(0).numpy()
+        transformed_mask = np.transpose(transformed_mask, (1, 2, 0))
+        
+        for t in reversed(replay_data['transforms']):
+        
+            transform_name = t['__class_fullname__']
+        
+            if 'Rotation' in transform_name and t["applied"]:
+                affine_matrix = t["params"]["matrix"]
+                if affine_matrix.shape == (3, 3):
+                    affine_matrix = affine_matrix[:2, :]
+
+                inverse_affine_matrix = cv2.invertAffineTransform(affine_matrix)
+                transformed_mask = cv2.warpAffine(transformed_mask, inverse_affine_matrix, (transformed_mask.shape[1], transformed_mask.shape[0]))
+                transformed_mask = transformed_mask[..., np.newaxis]
+
+            elif 'HorizontalFlip' in transform_name and t["applied"]:
+
+                transformed_mask = A.Compose([A.HorizontalFlip(p=1)])(image=transformed_mask)['image']
+                
+            elif 'VerticalFlip' in transform_name and t['applied']:
+                transformed_mask = A.Compose([A.VerticalFlip(p=1)])(image=transformed_mask)['image']
+        transformed_mask = torch.tensor(np.transpose(transformed_mask, (2, 0, 1)))
+
+        return transformed_mask
+    
+    def augment_and_predict(self, image, T):
+
+        """
+        :param image: Input image (C, H, W)
+        :param T: Number of augmentations to perform
+        """
+
+        image = np.transpose(image, (1, 2, 0))  # Convert to (H, W, C) for albumentations
+        # Store transformed images and masks
+        augmented_images = []
+        predicted_masks = []
+        replay_data_list = []
+
+        for _ in range(T):
+            augmented = self.transforms(image=image)
+            aug_image = augmented['image']
+            aug_image = torch.tensor(np.transpose(aug_image, (2, 0, 1)))
+            augmented_images.append(aug_image)
+            replay_data_list.append(augmented['replay'])  # Store augmentation params for reverse alignment
+
+            # Predict segmentation mask (add batch dimension)
+            with torch.no_grad():
+                mask_pred = self.model(aug_image.unsqueeze(0))
+
+            predicted_masks.append(mask_pred)
+
+        return predicted_masks, replay_data_list
+
+
+    def compute_entropy(self, masks):
+        # Stack along the new dimension to get shape [T, H, W] for pixel-wise computation
+        stacked_masks = torch.stack(masks, dim=0)
+        probs = torch.sigmoid(stacked_masks)  # Apply softmax if model outputs logits
+        entropy_map = entropy(probs.cpu().numpy(), axis=0)
+        return torch.tensor(entropy_map)
+
+    def evaluate(self, T = 5):
+        self.model.train(False)
+        self.model.eval()
+        
+
+        allInputs, allGT, allEntropies = [], [], []
+        for (images, GTs, resizedImg) in self.testDataLoader:
+            images      = images.to(self.device)
+
+            for image in images:
+                
+                predicted_masks, replay_data_list = self.augment_and_predict(image.data.numpy(), T)
+   
+                inverted_masks = [self.inverse_transform(mask, replay_data) for mask, replay_data in zip(predicted_masks, replay_data_list)]
+                entropy_map = self.compute_entropy(inverted_masks)
+
+                allEntropies.append(entropy_map)
+
+            allInputs.extend(resizedImg.data.numpy())
+            allGT.extend(GTs.data.numpy())
+
+        allInputs = np.array(allInputs)
+        allGT     = np.array(allGT)
+        allEntropies = np.array(allEntropies).squeeze()
+
+        showPredictions(allInputs, allEntropies, allGT, self.resultsPath)
+
+
+
+
+
+
+
